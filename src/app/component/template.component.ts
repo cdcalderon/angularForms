0a5a5e4be399854c491060a28ebf40dcd28ef5dc
carloscalderon <cdcalderon@gmail.com>
2017-12-02 15:03:13 -0600

    Add Validation to Template Form

--- src/app/component/template.component.ts
+++ src/app/component/template.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
+
+  user: any = {
+    firstName: null,
+    lastName: null,
+    email: null
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  save(form: NgForm) {
+    console.log('ngForm ' + form);
+
+    console.log('value ' + form.value);
+    console.log('user ' + this.user);
   }
 
 }
